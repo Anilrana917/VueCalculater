7b6e334e02ab581585df00fe57b488a194c98633
Anil rana <anilrana01.gh@gmail.com>
2021-12-01 16:15:57 +0530

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,97 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+<div id="Home">
+ <input v-model="inputData" type="text"> 
+ <br>
+ <button @click="additions" class="btn">+</button>
+ <button @click="deletion" class="btn">-</button>
+ <button @click="multiplication" class="btn">*</button>
+ <button @click="divided" class="btn">/</button>
+ <button @click="isEqual" class="btn">=</button>
+ <button @click="clear" class="btn">C</button>
+
+</div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  name: "Home",
+  data(){
+    return {
+      inputData: "", 
+      count:0,
+      typeofOperation: ""
+    }
+  },
+  watch: {
+  },
+
+  methods: {
+    additions(){
+      // console.log(this.inputData);
+      this.inputData = this.inputData.concat("+");
+      this.typeofOperation = "addition"
+    },
+    deletion(){
+      // console.log(this.inputData);
+      this.inputData = this.inputData.concat("-");
+      this.typeofOperation = "deletion"
+    },
+    multiplication(){
+      this.inputData = this.inputData.concat("*");
+       this.typeofOperation = "multiplication"
+    },
+    divided(){
+      this.inputData = this.inputData.concat("/");
+      this.typeofOperation = "divided"
+    },
+    isEqual(){
+      if(this.typeofOperation == "addition"){
+      let data = this.inputData.split("+");
+      let result = parseInt(data[0]) + parseInt(data[1])
+      this.inputData = this.inputData.concat("=");
+      this.inputData = this.inputData.concat(result)
+      }
+
+      else if (this.typeofOperation == "deletion"){
+      let data = this.inputData.split("-");
+      let result = parseInt(data[0]) - parseInt(data[1])
+      this.inputData = this.inputData.concat("=");
+      this.inputData = this.inputData.concat(result)
+      }
+
+      else if (this.typeofOperation == "multiplication"){
+      let data = this.inputData.split("*");
+      let result = parseInt(data[0]) * parseInt(data[1])
+      this.inputData = this.inputData.concat("=");
+      this.inputData = this.inputData.concat(result)
+      }
+
+      else if (this.typeofOperation == "divided"){
+      let data = this.inputData.split("/");
+      let result = parseInt(data[0]) / parseInt(data[1])
+      this.inputData = this.inputData.concat("=");
+      this.inputData = this.inputData.concat(result)
+      }
+
+      else{
+        console.log("I'm from else")
+      }
+      
+
+    },
+    clear(){
+      this.inputData = "";
+    }
   }
+  
+
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+<style>
+button.btn {
+    padding: 6px;
+    margin: 5px;
 }
 </style>
+
